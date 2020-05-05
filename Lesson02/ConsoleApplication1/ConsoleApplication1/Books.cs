e22580db31bb0e38adef58d3b0b4c5a073621d5b
ThomasSTA <tsta19@student.aau.dk>
2020-05-05 13:00:48 +0200

    Completed Lesson02

---
+++ Lesson02/ConsoleApplication1/ConsoleApplication1/Books.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApplication1.Properties
+{
+    public class Books
+    {
+        public string Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int NumberOfPages { get; set; }
+        public string Author { get; set; }
+    }
+}
---
+++ Lesson02/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Configuration;
+using ConsoleApplication1.Properties;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Books books = new Books();
+            
+            // Name Configuration
+            Console.WriteLine("Type the name of the book:");
+            books.Name = Console.ReadLine();
+            Console.Clear();
+            
+            // Release Date Configuration
+            Console.WriteLine("Type the release date of the book:");
+            books.ReleaseDate = DateTime.Parse(Console.ReadLine());
+            Console.Clear();
+            
+            // Number of Pages Configuration
+            Console.WriteLine("Type the number of pages the book has:");
+            books.NumberOfPages = Int16.Parse(Console.ReadLine());
+            Console.Clear();
+            
+            // Author Configuration
+            Console.WriteLine("Type the author of the book:");
+            books.Author = Console.ReadLine();
+            Console.Clear();
+            
+            // Summary
+            Console.WriteLine("------------------<[ Book Completion ]>------------------");
+            Console.WriteLine("The entered books name is: " + books.Name);
+            Console.WriteLine("The entered books release date is: " + books.ReleaseDate);
+            Console.WriteLine("The entered books number of pages is: " + books.NumberOfPages);
+            Console.WriteLine("The entered books author is: " + books.Author);
+            Console.WriteLine("------------------<[ Book Completion ]>------------------");
+            
+            Console.Read();
+
+        }
+    }
+}
