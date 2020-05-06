7fcc4b9111fcd248ef8831c859ccdaa650ed04a7
ThomasSTA <tsta19@student.aau.dk>
2020-05-07 01:24:06 +0200

    1

--- Course_Assignments/ConsoleApplication/ConsoleApplication/Book.cs
+++ Course_Assignments/ConsoleApplication/ConsoleApplication/Book.cs
@@ -8,5 +8,6 @@ namespace ConsoleApplication
         public DateTime ReleaseDate { get; set; }
         public int NumberOfPages { get; set; }
         public string Author { get; set; }
+        public Boolean InformaionIsCorrect { get; set; }
     }
 }
--- Course_Assignments/ConsoleApplication/ConsoleApplication/Program.cs
+++ Course_Assignments/ConsoleApplication/ConsoleApplication/Program.cs
@@ -10,35 +10,49 @@ namespace ConsoleApplication
         {
             Book book = new Book();
             BookRepository bookRepository = new BookRepository();
-            
+
             // Name Configuration
-            Console.WriteLine("Type the name of the book:");
+            Console.WriteLine("--------<[ Enter Book's Name ]>--------");
             book.Name = Console.ReadLine();
             Console.Clear();
             
             // Release Date Configuration
-            Console.WriteLine("Type the release date of the book:");
+            Console.WriteLine("--------<[ Enter Book's Release Date ]>--------");
             book.ReleaseDate = DateTime.Parse(Console.ReadLine());
             Console.Clear();
             
             // Number of Pages Configuration
-            Console.WriteLine("Type the number of pages the book has:");
+            Console.WriteLine("--------<[ Enter Book's Number of Pages ]>--------");
             book.NumberOfPages = int.Parse(Console.ReadLine());
             Console.Clear();
             
             // Author Configuration
-            Console.WriteLine("Type the author of the book:");
+            Console.WriteLine("--------<[ Enter Book's Author ]>--------");
             book.Author = Console.ReadLine();
             Console.Clear();
-            
+
             // Summary
             Console.WriteLine("------------------<[ Book Completion ]>------------------");
+            Console.WriteLine("");
             Console.WriteLine("The entered books name is: " + book.Name);
             Console.WriteLine("The entered books release date is: " + book.ReleaseDate);
             Console.WriteLine("The entered books number of pages is: " + book.NumberOfPages);
             Console.WriteLine("The entered books author is: " + book.Author);
+            Console.WriteLine("");
             Console.WriteLine("------------------<[ Book Completion ]>------------------");
-            bookRepository.Create(book);
+
+            if (Console.ReadLine() == "yes") {
+                book.InformaionIsCorrect = true;
+
+            } else if (Console.ReadLine() == "no") {
+                book.InformaionIsCorrect = false;
+            }
+
+            if (book.InformaionIsCorrect) {
+                Console.WriteLine("Uploading book to database....");
+                bookRepository.Create(book);
+                Console.WriteLine("Book uploaded successfully!");
+            }
             
             Console.Read();
         }
